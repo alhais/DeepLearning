032eedeb546da6b5155f6d78a5421858b0fb5a09
alhais <42003904+alhais@users.noreply.github.com>
2018-08-03 19:21:33 +0100

    Update EdgeTransformation.py

--- EdgeTransformation.py
+++ EdgeTransformation.py
@@ -1,12 +1,13 @@
 import os
+import sys
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-images_dir = '/content/pix2pix-tensorflow/pix2pix-tensorflow/DeepLearning/DeepLearning/DeepLearning/datasets'
+images_dir = os.getcwd() + '/datasets/video_jpegs'
 imgList = os.listdir(images_dir)
 nImgs = len(imgList)
 print('#images = %d' % nImgs)
-img = cv.imread(imgList[200],0)
+img = cv.imread(images_dir+ '/' + imgList[200],0)
 edges = cv.Canny(img,100,200)
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
@@ -15,8 +16,10 @@ plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 plt.show()
 cv.imwrite('first.jpg',edges)
 
+os.makedirs(os.getcwd() + '/datasets/video_jpegs/edge_videos')
+
 for i in range(nImgs):
   img = cv.imread(imgList[i],0)
   edges = cv.Canny(img,100,200)
   print('%s' % 'edge_'+imgList[i])
-  cv.imwrite('edge_'+imgList[i],edges)
+  cv.imwrite(images_dir + '/edge_videos/' + 'edge_'+imgList[i],edges)
