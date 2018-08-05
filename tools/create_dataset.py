b0678f5d6f926de65621a587890311ef00422043
alhais <42003904+alhais@users.noreply.github.com>
2018-08-05 18:24:18 +0100

    Update create_dataset.py

--- tools/create_dataset.py
+++ tools/create_dataset.py
@@ -1,15 +1,17 @@
+import os
 import cv2
 import numpy as np
 print(cv2.__version__)
-vidcap = cv2.VideoCapture('0.mp4')
+vidcap = cv2.VideoCapture(os.getcwd() + '/datasets/0.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
+os.makedirs(os.getcwd() + '/datasets/train')
 while success:
   edges = cv2.Canny(image,100,200)
   color_edges = cv2.cvtColor(edges, 8)
   vis = np.concatenate((image, color_edges), axis=1)
   resized_image = cv2.resize(vis, (512, 256)) 
-  cv2.imwrite("%d.jpg" % count, resized_image)     # save frame as JPEG file
+  cv2.imwrite(os.getcwd() + '/datasets/train' + "%d.jpg" % count, resized_image)     # save frame as JPEG file
   success,image = vidcap.read()
   count += 1
