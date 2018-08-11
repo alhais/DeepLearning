fb5da185078ce18d23e810404a4028002b09c24a
alhais <42003904+alhais@users.noreply.github.com>
2018-08-11 11:16:30 +0100

    Update data_loader.py

--- pix2pix/data_loader.py
+++ pix2pix/data_loader.py
@@ -10,29 +10,34 @@ class DataLoader():
 
     def load_data(self, batch_size=1, is_testing=False):
         data_type = "train" if not is_testing else "test"
-        path = glob('./datasets/%s/%s/*' % (self.dataset_name, data_type))
+        batch_images_A = glob('./datasets/%s/%s/A/*' % (self.dataset_name, data_type))
+        batch_images_B = glob('./datasets/%s/%s/B/*' % (self.dataset_name, data_type))
 
-        batch_images = np.random.choice(path, size=batch_size)
+
+        #batch_images = np.random.choice(path, size=batch_size)
 
         imgs_A = []
         imgs_B = []
-        for img_path in batch_images:
-            img = self.imread(img_path)
-
-            h, w, _ = img.shape
-            _w = int(w/2)
-            img_A, img_B = img[:, :_w, :], img[:, _w:, :]
-
+        for img_path in batch_images_A:
+            img_A = self.imread(img_path)
             img_A = scipy.misc.imresize(img_A, self.img_res)
-            img_B = scipy.misc.imresize(img_B, self.img_res)
 
             # If training => do random flip
             if not is_testing and np.random.random() < 0.5:
                 img_A = np.fliplr(img_A)
-                img_B = np.fliplr(img_B)
 
             imgs_A.append(img_A)
+            
+         for img_path in batch_images_B:
+            img_B = self.imread(img_path)
+            img_B = scipy.misc.imresize(img_B, self.img_res)
+
+            # If training => do random flip
+            if not is_testing and np.random.random() < 0.5:
+                img_B = np.fliplr(img_B)
+
             imgs_B.append(img_B)
+            
 
         imgs_A = np.array(imgs_A)/127.5 - 1.
         imgs_B = np.array(imgs_B)/127.5 - 1.
@@ -41,29 +46,40 @@ class DataLoader():
 
     def load_batch(self, batch_size=1, is_testing=False):
         data_type = "train" if not is_testing else "val"
-        path = glob('./datasets/%s/%s/*' % (self.dataset_name, data_type))
+        pathA = glob('./datasets/%s/%s/A/*' % (self.dataset_name, data_type))
+        pathB = glob('./datasets/%s/%s/B/*' % (self.dataset_name, data_type))
 
         self.n_batches = int(len(path) / batch_size)
 
         for i in range(self.n_batches-1):
-            batch = path[i*batch_size:(i+1)*batch_size]
-            imgs_A, imgs_B = [], []
+            batch = pathA[i*batch_size:(i+1)*batch_size]
+            imgs_A = []
             for img in batch:
-                img = self.imread(img)
-                h, w, _ = img.shape
-                half_w = int(w/2)
-                img_A = img[:, :half_w, :]
-                img_B = img[:, half_w:, :]
+                img_A = self.imread(img)
 
                 img_A = scipy.misc.imresize(img_A, self.img_res)
-                img_B = scipy.misc.imresize(img_B, self.img_res)
+
 
                 if not is_testing and np.random.random() > 0.5:
                         img_A = np.fliplr(img_A)
-                        img_B = np.fliplr(img_B)
+                        
 
                 imgs_A.append(img_A)
+          for i in range(self.n_batches-1):
+            batch = pathB[i*batch_size:(i+1)*batch_size]
+            imgs_B = []
+            for img in batch:
+                img_B = self.imread(img)
+
+                img_B = scipy.misc.imresize(img_B, self.img_res)
+
+
+                if not is_testing and np.random.random() > 0.5:
+                        img_B = np.fliplr(img_B)
+                        
+
                 imgs_B.append(img_B)
+                
 
             imgs_A = np.array(imgs_A)/127.5 - 1.
             imgs_B = np.array(imgs_B)/127.5 - 1.
