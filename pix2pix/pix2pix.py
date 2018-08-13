d9e187635fa5520aa20d92cd4fdc5d4d2bc0aa5c
alhais <42003904+alhais@users.noreply.github.com>
2018-08-13 18:44:53 +0100

    Update pix2pix.py

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -241,7 +241,7 @@ class Pix2Pix():
         for i in range(r):
             for j in range(c):
                 axs[i,j].imshow(gen_imgs[cnt])
-                axs[i, j].set_title(titles[i])
+                axs[i, j].set_title(titles[i],fontsize=8)
                 axs[i,j].axis('off')
                 cnt += 1
         fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
