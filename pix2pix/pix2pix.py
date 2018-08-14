91bf1cd842ecb6d4d576bc0be3934c157ba8bd24
alhais <42003904+alhais@users.noreply.github.com>
2018-08-14 11:58:25 +0100

    Update pix2pix.py

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -105,8 +105,8 @@ class Pix2Pix():
         # Downsampling
         h = conv2d(input_EMG, self.gf, bn=False)
         h = conv2d(h, self.gf*2)
-        #h = MaxPooling2D((2, 2), strides=(1, 2), padding='same')(h)
-        #h = conv2d(h, self.gf*4)
+        h = MaxPooling2D((2, 2), strides=(1, 2), padding='same')(h)
+        h = conv2d(h, self.gf*4)
         #h = conv2d(h, self.gf*8)
         #h = conv2d(h, self.gf*8)
         h = MaxPooling2D((2, 2), strides=(2, 2), padding='same')(h)
