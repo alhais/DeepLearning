d5eae84058b660cf599f126dfb34365207f3aa10
alhais <42003904+alhais@users.noreply.github.com>
2018-08-14 19:13:39 +0100

    Update pix2pix.py

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -66,7 +66,7 @@ class Pix2Pix():
         [I0, Z0] = self.generator([I, img_B])
 
         # For the combined model we will only train the generator
-        self.discriminator.trainable = True
+        self.discriminator.trainable = False
 
 
         # Discriminators determines validity of translated images / condition pairs
@@ -236,8 +236,8 @@ class Pix2Pix():
 
                 # Train the discriminators (original images = real / generated = Fake)
                 d_loss_real = self.discriminator.train_on_batch([imgs_A, Z0], [valid, match])
-                #d_loss_fake = self.discriminator.train_on_batch([fake_A, Z0], [fake, fake_match])
-                d_loss_fake = 0
+                d_loss_fake = self.discriminator.train_on_batch([fake_A, Z0], [fake, fake_match])
+                #d_loss_fake = 0
                 d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
                 # -----------------
