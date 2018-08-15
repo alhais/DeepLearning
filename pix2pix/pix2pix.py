c091e9d3f802235eca4b97350fa5b763f6eef915
alhais <42003904+alhais@users.noreply.github.com>
2018-08-15 21:32:30 +0100

    Update pix2pix.py

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -84,10 +84,8 @@ class Pix2Pix():
         [valid, match] = self.discriminator([I0, Z0])
 
         
-        final_model = Sequential()
-        final_model.add(I)
-        final_model.add(Z0)   
-        final_model.summary()    
+        g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(I0, tf.ones_like(Z0)))  
+           
             
         self.combined = Model(inputs=[img_A, I, img_B], outputs=[valid, match, I0, Z0])
         self.combined.compile(loss=[custom_loss, custom_loss,custom_loss,custom_loss],
