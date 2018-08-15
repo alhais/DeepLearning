16c10f35f2920b061512d18546cacc978772cc2f
alhais <42003904+alhais@users.noreply.github.com>
2018-08-15 19:45:24 +0100

    Update pix2pix.py

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -24,21 +24,14 @@ class Pix2Pix():
                   
        
         def D0_loss(y_true, y_pred):
-            sess = tf.InteractiveSession()
-            print(y_true.eval())
-            sess.close()
             return K.mean(K.square(y_pred - y_true), axis=-1)#K.log(1.0 - y_pred) + K.log(1.0 - y_true)
         def D1_loss(y_true, y_pred):
-            print(y_true.shape)
             return K.mean(K.square(y_pred - y_true), axis=-1)#K.log(1.0 - y_pred) + K.log(1.0 - y_true)
         
         def G0_loss(y_true, y_pred):
-            result = []
-            print(y_true.shape)
             return K.mean(K.square(y_pred - y_true), axis=-1)#K.log(1.0 - y_pred) + K.log(1.0 - y_true)
 
         def G1_loss(y_true, y_pred):
-            print(y_true.shape)
             return K.mean(K.square(y_pred - y_true), axis=-1)#K.log(1.0 - y_pred) + K.log(1.0 - y_true)
                          
         # Input shape
