fc3ae654efc79f94a6566fbdb789522472e9e530
alhais <42003904+alhais@users.noreply.github.com>
2018-08-17 17:53:02 +0100

    Update pix2pix.py

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -288,7 +288,7 @@ class Pix2Pix():
  
                 # Train the discriminators (original images = real / generated = Fake)
                 d_loss_real = self.discriminator.train_on_batch([imgs_A, Z0], [valid, match])
-                d_loss_fake = self.discriminator.train_on_batch([fake_A, Z0], [fake, fake_match])
+                d_loss_fake = self.discriminator.train_on_batch([fake_A, Z0], [fake, match])
                 d_loss_mismatch = self.discriminator.train_on_batch([imgs_A, fakes_Z0], [valid, fake_match])
                 
 
