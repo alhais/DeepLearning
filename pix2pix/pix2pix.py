ad3013ef17ce818f08990f085ffb55b164a68cce
alhais <42003904+alhais@users.noreply.github.com>
2018-08-18 22:18:22 +0100

    Update pix2pix.py

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -106,7 +106,7 @@ class Pix2Pix():
         
         #Get new model already with trainned encoder decoder
         self.generator = Model(inputs=self.CAE.input, outputs=[self.CAE.get_layer('output').output, self.CAE.get_layer('z0').output])
-        self.generator.trainable = False
+        self.generator.trainable = True
 
         # Input images and their conditioning images
         I = Input(shape=self.img_shape)
